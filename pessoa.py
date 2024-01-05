7270ba72c81ff23212a48cd545c408fa1b33227d
Fabs san <fabianagithub@gmail.com>
2024-01-05 19:52:04 +0000

    teste

---
+++ pessoa.py
@@ -0,0 +1,31 @@
+class Pessoa:
+
+    def __init__ (self,nome,cpf,id):
+        self.nome = nome
+        self.cpf = cpf
+        self.id = id
+
+class vendedor:
+
+    def __init__ (Pessoa, nome, salario, vendedor_id):
+        # inicializa os atributos comuns da classe pessoa
+        super().__init__(vendedor_id, nome)
+        self.cpf = cpf
+
+class VendedorDataBase:
+    def __init__(self):
+        self.vendedor = []
+
+
+class criar_vendedor:
+    def __init__(vendedor_id, nome, cpf):
+        novo.vendedor = vendedor(vendedor_id, nome, cpf)
+        self.vendedor.append(novo_vendedor)
+        if (nome >= 4):
+            print(f"vendedor {name} adicionado em database.")
+        else:
+            print(f"vendedor {name} invalido")
+
+
+    
+    
